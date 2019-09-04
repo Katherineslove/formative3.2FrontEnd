97f8c9f8cc3b81361f1bd0b02422349adaba3a05
Andy Ball <932400399@bun2-206-11.local>
2019-09-05 10:48:08 +1200

    ajax request for adding projects started

--- js/script.js
+++ js/script.js
@@ -1,1 +1,52 @@
-console.log('working');
+$(document).ready(function(){
+	let serverURL;
+	let serverPort;
+	let url;
+
+	// get configs and request all projects
+	$.ajax({
+    url: 'config.json',
+    type: 'GET',
+    dataType: 'json',
+    success:function(keys){
+      serverURL = keys['SERVER_URL'];
+      serverPort = keys['SERVER_PORT'];
+      url = `${serverURL}:${serverPort}`;
+      // getProjects();
+    },
+    error: function(){
+      console.log('cannot find config.json file, cannot run application');
+    }
+  });
+
+	// do stuff when the add product button is clicked
+	$('#addProductButton').click(function(){
+		event.preventDefault();
+
+		let project_id;
+		let author_id;
+		let author_name;
+		let project_name;
+		let screenshotURL;
+		let link;
+
+		$.ajax({
+			url: `${url}/addProject`,
+			type: 'POST',
+			data: {
+				projectId: project_id,
+				// authorId: author_id,
+				authorName: author_name,
+				projectName: project_name,
+				screenshotURL: screenshot_URL
+			},
+			success: function(data){
+				console.log(data);
+				},
+			error: function(err){
+				console.log('got an error!');
+				console.log(err);
+			}
+		});
+	});
+});
